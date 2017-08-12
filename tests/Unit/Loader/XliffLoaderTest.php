fdfeb3691ef470c882c70f5d87f786b1e9eaeea0
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-08-12 20:19:08 +0200

    Added tests for XliffLoader

---
+++ tests/Unit/Loader/XliffLoaderTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Translation\SymfonyStorage\Tests\Unit\Loader;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Translation\Exception\InvalidResourceException;
+use Symfony\Component\Translation\MessageCatalogue;
+use Translation\SymfonyStorage\Loader\XliffLoader;
+
+/**
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class XliffLoaderTest extends TestCase
+{
+    /**
+     * @expectedException \Symfony\Component\Translation\Exception\InvalidResourceException
+     */
+    public function testEmptyContent()
+    {
+        $loader = new XliffLoader();
+        $loader->extractFromContent(' ', new MessageCatalogue('en'), 'messages');
+    }
+
+    public function testInvalidContent()
+    {
+        $loader = new XliffLoader();
+
+        try {
+            $loader->extractFromContent('Foobar', new MessageCatalogue('en'), 'messages');
+        } catch (InvalidResourceException $e) {
+            $invalidArgument = $e->getPrevious();
+            $this->assertNotNull($invalidArgument);
+            $this->assertContains('[ERROR 4] Start tag expected', $invalidArgument->getMessage());
+
+            return;
+        }
+        $this->fail('XliffLoader must throw exception on invalid XML');
+    }
+
+    public function testXliff12()
+    {
+        $content = file_get_contents(__DIR__.'/../../Fixtures/single-file/messages.en.xlf');
+        $catalogue = new MessageCatalogue('en');
+        (new XliffLoader())->extractFromContent($content, $catalogue, 'messages');
+        $this->assertTrue($catalogue->defines('test_0'));
+        $this->assertTrue($catalogue->defines('test_1'));
+    }
+
+    public function testXliff20()
+    {
+        $content=<<<XML
+<xliff xmlns="urn:oasis:names:tc:xliff:document:2.0" version="2.0"
+ srcLang="en-US" trgLang="sv">
+ <file id="f1" original="Example">
+  <skeleton href="Example"/>
+  <unit id="1">
+   <segment>
+    <source>key0</source>
+    <target>Foo</target>
+   </segment>
+  </unit>
+  <unit id="2">
+   <segment>
+    <source>key1</source>
+    <target>Bar</target>
+   </segment>
+  </unit>
+ </file>
+</xliff>
+XML;
+
+        $catalogue = new MessageCatalogue('en');
+        (new XliffLoader())->extractFromContent($content, $catalogue, 'messages');
+        $this->assertTrue($catalogue->defines('key0'));
+        $this->assertTrue($catalogue->defines('key1'));
+    }
+}
